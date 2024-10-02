9df8d070f774cd01a83b49d3150e57ed617f2c30
michel-fdzzz <michel.fdzzz04@gmail.com>
2024-10-02 12:28:45 +0200

    validar los campos del formulario

--- src/app/components/form-item/form-item.component.ts
+++ src/app/components/form-item/form-item.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'form-item',
@@ -18,7 +18,9 @@ export class FormItemComponent {
     quantity: ''
   }
 
-  onSubmit(): void {
+  onSubmit(itemForm: NgForm): void {
+    //if (itemForm.valid) {
+
     this.addItemEventEmitter.emit({ id: this.counterId, ... this.item });
     this.counterId++;
 
@@ -28,5 +30,10 @@ export class FormItemComponent {
       price: '',
       quantity: ''
     }
+    itemForm.reset();
+    itemForm.resetForm();
+    // }
+
+
   }
 }
